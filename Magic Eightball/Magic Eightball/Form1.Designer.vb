32c72be7b4318fb53b54364c5725c8d3464f1263
ehqua <ehquamcfly@gmail.com>
2026-02-04 12:03:16 -0800

    Add files via upload

---
+++ Magic Eightball/Magic Eightball/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClickClack = New Button()
+        Label1 = New Label()
+        tbx1to20 = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnClickClack
+        ' 
+        btnClickClack.Location = New Point(349, 187)
+        btnClickClack.Name = "btnClickClack"
+        btnClickClack.Size = New Size(75, 23)
+        btnClickClack.TabIndex = 0
+        btnClickClack.Text = "Button1"
+        btnClickClack.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(349, 93)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Maigc woah"
+        ' 
+        ' tbx1to20
+        ' 
+        tbx1to20.Location = New Point(333, 133)
+        tbx1to20.Name = "tbx1to20"
+        tbx1to20.Size = New Size(100, 23)
+        tbx1to20.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(363, 232)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Label2"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(tbx1to20)
+        Controls.Add(Label1)
+        Controls.Add(btnClickClack)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnClickClack As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbx1to20 As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Magic Eightball/Magic Eightball/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Private Sub btnClickClack_Click(sender As Object, e As EventArgs) Handles btnClickClack.Click
+        Dim RNG As New Random
+        Dim strNumber As String
+        strNumber = RNG.Next(1, 21)
+        strNumber = Label2.Text
+        Select Case RNG.Next(1, 21)
+            Case 1
+                Label2.Text = "It is certain"
+            Case 2
+                Label2.Text = "It is decidedly so"
+            Case 3
+                Label2.Text = "Without a doubt"
+            Case 4
+                Label2.Text = "Yes definitely"
+            Case 5
+                Label2.Text = "You may rely on it"
+            Case 6
+                Label2.Text = "As I see it. yes"
+            Case 7
+                Label2.Text = "Most likely"
+            Case 8
+                Label2.Text = "Outlook good"
+            Case 9
+                Label2.Text = "Yes"
+            Case 10
+                Label2.Text = "Signs point to yes"
+            Case 11
+                Label2.Text = "Reply hazy, try again"
+            Case 12
+                Label2.Text = "Ask again later"
+            Case 13
+                Label2.Text = "Better not to tell you now"
+            Case 14
+                Label2.Text = "Cannot predict now"
+            Case 15
+                Label2.Text = "Concentrate and ask again"
+            Case 16
+                Label2.Text = "Don't count on it"
+            Case 17
+                Label2.Text = "My reply is no"
+            Case 18
+                Label2.Text = "My sources say no"
+            Case 19
+                Label2.Text = "Outlook not so good"
+            Case 20
+                Label2.Text = "Very Doubtful"
+        End Select
+
+
+    End Sub
+End Class
