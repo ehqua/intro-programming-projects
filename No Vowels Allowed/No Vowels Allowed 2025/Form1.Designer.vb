858a216df3946399782d3471c074cf1b9c08f825
ehqua <ehquamcfly@gmail.com>
2026-02-04 12:06:03 -0800

    Add files via upload

---
+++ No Vowels Allowed/No Vowels Allowed 2025/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        RichTextBox1 = New RichTextBox()
+        RichTextBox2 = New RichTextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(243, 114)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(100, 96)
+        RichTextBox1.TabIndex = 0
+        RichTextBox1.Text = ""
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.Location = New Point(413, 114)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(100, 96)
+        RichTextBox2.TabIndex = 1
+        RichTextBox2.Text = ""
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(335, 62)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(RichTextBox2)
+        Controls.Add(RichTextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ No Vowels Allowed/No Vowels Allowed 2025/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strOriginal As String
+        Dim strNew As String
+        strOriginal = RichTextBox1.Text
+        For index = 0 To strOriginal.Length() - 1
+            Dim check As Char = strOriginal(index)
+            If check <> "a" And check <> "e" And check <> "i" And check <> "o" And check <> "u" And check <> "A" And check <> "E" And check <> "I" And check <> "O" And check <> "U" Then
+                strNew &= check
+            End If
+        Next
+        RichTextBox2.Text = strNew
+    End Sub
+End Class
